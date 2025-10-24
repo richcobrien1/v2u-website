2d1f0e8b93ca69a90f4a954429ee9610de21cf43
richcobrien1 <richcobrien@hotmail.com>
2025-10-24 14:44:17 -0600

    one more

--- app/founder-subscriber/page.tsx
+++ app/founder-subscriber/page.tsx
@@ -24,7 +24,7 @@ export default function FounderSubscriberPage() {
 
   return (
     <main className="w-full h-auto pt-12 bg-bgLight dark:bg-bgDark text-black dark:text-white transition-colors duration-300">
-      <Header loggedIn={true} firstName="Welcome" avatar="🟡" />
+      <Header />
 
       <div className="px-4 md:px-4 space-y-4">
 
