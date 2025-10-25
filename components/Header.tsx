522a9591a8bc87eead3b64b7ef34178e246ed48a
richcobrien1 <richcobrien@hotmail.com>
2025-10-25 14:07:55 -0600

    update header for login

--- components/Header.tsx
+++ components/Header.tsx
@@ -173,7 +173,7 @@ export default function Header({
                     Join
                   </button>
                   <Link
-                    href="/podcast-dashboard"
+                    href="/login"
                     className={`rounded-md ${buttonBg} px-3 py-1.5 text-sm ${hoverBg}`}
                   >
                     Login
