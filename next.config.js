b47605017ce30e8a0199c02d8f280f96a10a78d6
Rich O'Brien <richcobrien1@gmail.com>
2026-03-18 20:39:14 -0600

    fix: Add Facebook Pixel to CSP allowed sources

--- next.config.js
+++ next.config.js
@@ -16,11 +16,11 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net;
               style-src 'self' 'unsafe-inline';
               img-src 'self' data: https: blob:;
               font-src 'self' data:;
-              connect-src 'self' https://js.stripe.com https://api.stripe.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
+              connect-src 'self' https://js.stripe.com https://api.stripe.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://*.facebook.com https://*.facebook.net;
               frame-src 'self' https://js.stripe.com https://hooks.stripe.com;
               object-src 'none';
               base-uri 'self';
