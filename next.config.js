ea3c236f75ac3abe4bc9980433d9e5fc59cdd35a
Rich O'Brien <richcobrien1@gmail.com>
2026-03-31 11:19:29 -0600

    fix: add open.spotify.com to CSP frame-src

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const nextConfig = {
               img-src 'self' data: https: blob:;
               font-src 'self' data: https://clerk.v2u.us https://*.clerk.accounts.dev;
               connect-src 'self' https://js.stripe.com https://api.stripe.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://*.facebook.com https://*.facebook.net https://*.r2.cloudflarestorage.com https://clerk.v2u.us https://*.clerk.accounts.dev https://api.clerk.com https://*.clerkstage.dev;
-              frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://www.youtube.com https://www.youtube-nocookie.com https://www.google.com https://maps.google.com https://clerk.v2u.us https://*.clerk.accounts.dev https://challenges.cloudflare.com;
+              frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://www.youtube.com https://www.youtube-nocookie.com https://www.google.com https://maps.google.com https://clerk.v2u.us https://*.clerk.accounts.dev https://challenges.cloudflare.com https://open.spotify.com;
               worker-src 'self' blob:;
               media-src 'self' blob: data: https:;
               object-src 'none';
