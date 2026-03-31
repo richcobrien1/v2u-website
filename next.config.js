ea56c0cec6dc50a18f1d4a1b0635f0eeaad43566
Rich O'Brien <richcobrien1@gmail.com>
2026-03-31 09:19:32 -0600

    fix: add Clerk domains to Content Security Policy

--- next.config.js
+++ next.config.js
@@ -16,16 +16,17 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net;
-              style-src 'self' 'unsafe-inline';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.stripe.com https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net https://clerk.v2u.us https://*.clerk.accounts.dev https://challenges.cloudflare.com;
+              style-src 'self' 'unsafe-inline' https://clerk.v2u.us https://*.clerk.accounts.dev;
               img-src 'self' data: https: blob:;
-              font-src 'self' data:;
-              connect-src 'self' https://js.stripe.com https://api.stripe.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://*.facebook.com https://*.facebook.net https://*.r2.cloudflarestorage.com;
-              frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://www.youtube.com https://www.youtube-nocookie.com https://www.google.com https://maps.google.com;
+              font-src 'self' data: https://clerk.v2u.us https://*.clerk.accounts.dev;
+              connect-src 'self' https://js.stripe.com https://api.stripe.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com https://*.facebook.com https://*.facebook.net https://*.r2.cloudflarestorage.com https://clerk.v2u.us https://*.clerk.accounts.dev https://api.clerk.com https://*.clerkstage.dev;
+              frame-src 'self' https://js.stripe.com https://hooks.stripe.com https://www.youtube.com https://www.youtube-nocookie.com https://www.google.com https://maps.google.com https://clerk.v2u.us https://*.clerk.accounts.dev https://challenges.cloudflare.com;
+              worker-src 'self' blob:;
               media-src 'self' blob: data: https:;
               object-src 'none';
               base-uri 'self';
-              form-action 'self';
+              form-action 'self' https://clerk.v2u.us https://*.clerk.accounts.dev;
               frame-ancestors 'self';
               upgrade-insecure-requests;
             `.replace(/\s{2,}/g, ' ').trim()
